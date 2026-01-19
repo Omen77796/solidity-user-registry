281226827e9494de8efe3fc61798e10a325e52c9
THE_CybOrg_777 <124538034+Omen77796@users.noreply.github.com>
2026-01-19 16:07:03 -0500

    Add Voting contract with proposals and voting logic
    Implement a voting contract with proposal management and voting functionality.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,1 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        string name;
+        uint voteCount;
+    }
+
+    Proposal[] public proposals;
+    mapping(address => bool) public hasVoted;
+
+    event ProposalCreated(uint id, string name);
+    event VoteCast(address voter, uint proposalId);
+
+    constructor(string[] memory proposalNames) {
+        for (uint i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+
+            emit ProposalCreated(i, proposalNames[i]);
+        }
+    }
+
+    function vote(uint proposalId) public {
+        require(!hasVoted[msg.sender], "Ya votaste");
+        require(proposalId < proposals.length, "Propuesta invalida");
+
+        hasVoted[msg.sender] = true;
+        proposals[proposalId].voteCount += 1;
+
+        emit VoteCast(msg.sender, proposalId);
+    }
+
+    function getProposalsCount() public view returns (uint) {
+        return proposals.length;
+    }
+}
 
